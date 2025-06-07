c8087f6a332cccd22b8b8f3ac070e8261847cc6f
DIARY7 <diaryhakeur@gmail.com>
2025-06-07 18:09:20 +0300

    exposition create

---
+++ hotel-management/hotel/controllers/rest_controller.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# from odoo import http
+#
+#
+# class RestController(http.Controller):
+#
+#     @http.route('/api/room-booking/create', auth='public', methods=['POST'], type='json', csrf=False)
+#     def create_booking(self, **post):
+
+
